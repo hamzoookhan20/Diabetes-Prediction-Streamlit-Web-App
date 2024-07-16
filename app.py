018c1f477edbc0ee7a0c0265d5eac406ca42b5d6
Muhammad Hamza <62422916+hamzoookhan20@users.noreply.github.com>
2024-07-16 21:02:26 +0500

    Update app.py

--- app.py
+++ app.py
@@ -44,25 +44,25 @@ if (selected == 'Diabetes Prediction'):
         Pregnancies = st.text_input('Number of Pregnancies',help="Enter the number of times the patient has been pregnant.")
         
     with col2:
-        Glucose = st.text_input('Glucose Level')
+        Glucose = st.text_input('Glucose Level',help="Enter the plasma glucose concentration measured two hours after an oral glucose tolerance test.")
     
     with col3:
-        BloodPressure = st.text_input('Blood Pressure value')
+        BloodPressure = st.text_input('Blood Pressure value',help="Enter the diastolic blood pressure value in mm Hg. This is the blood pressure when the heart is resting between beats.")
     
     with col1:
-        SkinThickness = st.text_input('Skin Thickness value')
+        SkinThickness = st.text_input('Skin Thickness value',help="Enter the thickness of the triceps skin fold in mm. This value helps in estimating body fat.")
     
     with col2:
-        Insulin = st.text_input('Insulin Level')
+        Insulin = st.text_input('Insulin Level',help="Enter the 2-hour serum insulin level in mu U/ml. This helps in assessing insulin production and resistance.")
     
     with col3:
-        BMI = st.text_input('BMI value')
+        BMI = st.text_input('BMI value',help="Enter the Body Mass Index (BMI) value, calculated as weight in kg divided by the square of height in meters. It indicates body fat and health risk.")
     
     with col1:
-        DiabetesPedigreeFunction = st.text_input('Diabetes Pedigree Function value')
+        DiabetesPedigreeFunction = st.text_input('Diabetes Pedigree Function value',help="Enter the diabetes pedigree function value, a measure of the genetic risk of diabetes based on family history.")
     
     with col2:
-        Age = st.text_input('Age of the Person')
+        Age = st.text_input('Age of the Person',help="Enter the age of the patient in years. This value should be a whole number.")
     
     
     # code for Prediction
