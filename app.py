b200be8e31a90399ff9829989e05e2d87ec26c5a
Muhammad Hamza <62422916+hamzoookhan20@users.noreply.github.com>
2024-07-16 22:26:31 +0500

    Update app.py

--- app.py
+++ app.py
@@ -84,6 +84,7 @@ if (selected == 'Diabetes Prediction'):
                     diab_diagnosis = 'The person is not diabetic'
     st.success(diab_diagnosis)
     st.write("Please fill in all the input fields with the appropriate values to get an accurate prediction.")
+    st.info('This is a purely informational message', icon="ℹ️")
     st.balloons()
 
 
