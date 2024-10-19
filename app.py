942af179a6f2ea4f87083d2635334c90ce50903d
Muhammad Hamza <62422916+hamzoookhan20@users.noreply.github.com>
2024-10-19 18:44:51 +0500

    Update app.py

--- app.py
+++ app.py
@@ -8,9 +8,17 @@ import io
 # Load diabetes_model.pkl file
 diabetes_model = pickle.load(open('diabetes_model.pkl', 'rb'))
 
-# Initialize session state for diabetic status
+# Initialize session state for input values and diabetic status
 if 'diabetic' not in st.session_state:
     st.session_state['diabetic'] = False
+    st.session_state['Pregnancies'] = ''
+    st.session_state['Glucose'] = ''
+    st.session_state['BloodPressure'] = ''
+    st.session_state['SkinThickness'] = ''
+    st.session_state['Insulin'] = ''
+    st.session_state['BMI'] = ''
+    st.session_state['DiabetesPedigreeFunction'] = ''
+    st.session_state['Age'] = ''
     st.session_state['features'] = None  # To store the input features
     st.session_state['feature_importance'] = None  # To store the importance of features
 
@@ -38,32 +46,32 @@ if selected == 'Diabetes Prediction':
     # Page title
     st.title('Predicting Diabetes Onset using ML')
     
-    # Getting the input data from the user
+    # Getting the input data from the user, pre-filling with session state values
     col1, col2, col3 = st.columns(3)
     
     with col1:
-        Pregnancies = st.text_input('Number of Pregnancies', help="Enter the number of times the patient has been pregnant.")
+        st.session_state['Pregnancies'] = st.text_input('Number of Pregnancies', value=st.session_state['Pregnancies'], help="Enter the number of times the patient has been pregnant.")
         
     with col2:
-        Glucose = st.text_input('Glucose Level', help="Enter the plasma glucose concentration measured two hours after an oral glucose tolerance test.")
+        st.session_state['Glucose'] = st.text_input('Glucose Level', value=st.session_state['Glucose'], help="Enter the plasma glucose concentration measured two hours after an oral glucose tolerance test.")
     
     with col3:
-        BloodPressure = st.text_input('Blood Pressure value', help="Enter the diastolic blood pressure value in mm Hg. This is the blood pressure when the heart is resting between beats.")
+        st.session_state['BloodPressure'] = st.text_input('Blood Pressure value', value=st.session_state['BloodPressure'], help="Enter the diastolic blood pressure value in mm Hg. This is the blood pressure when the heart is resting between beats.")
     
     with col1:
-        SkinThickness = st.text_input('Skin Thickness value', help="Enter the thickness of the triceps skin fold in mm. This value helps in estimating body fat.")
+        st.session_state['SkinThickness'] = st.text_input('Skin Thickness value', value=st.session_state['SkinThickness'], help="Enter the thickness of the triceps skin fold in mm. This value helps in estimating body fat.")
     
     with col2:
-        Insulin = st.text_input('Insulin Level', help="Enter the 2-hour serum insulin level in mu U/ml. This helps in assessing insulin production and resistance.")
+        st.session_state['Insulin'] = st.text_input('Insulin Level', value=st.session_state['Insulin'], help="Enter the 2-hour serum insulin level in mu U/ml. This helps in assessing insulin production and resistance.")
     
     with col3:
-        BMI = st.text_input('BMI value', help="Enter the Body Mass Index (BMI) value, calculated as weight in kg divided by the square of height in meters. It indicates body fat and health risk.")
+        st.session_state['BMI'] = st.text_input('BMI value', value=st.session_state['BMI'], help="Enter the Body Mass Index (BMI) value, calculated as weight in kg divided by the square of height in meters. It indicates body fat and health risk.")
     
     with col1:
-        DiabetesPedigreeFunction = st.text_input('Diabetes Pedigree Function value', help="Enter the diabetes pedigree function value, a measure of the genetic risk of diabetes based on family history.")
+        st.session_state['DiabetesPedigreeFunction'] = st.text_input('Diabetes Pedigree Function value', value=st.session_state['DiabetesPedigreeFunction'], help="Enter the diabetes pedigree function value, a measure of the genetic risk of diabetes based on family history.")
     
     with col2:
-        Age = st.text_input('Age of the Person', help="Enter the age of the patient in years. This value should be a whole number.")
+        st.session_state['Age'] = st.text_input('Age of the Person', value=st.session_state['Age'], help="Enter the age of the patient in years. This value should be a whole number.")
     
     # Code for Prediction
     diab_diagnosis = ''
@@ -72,20 +80,20 @@ if selected == 'Diabetes Prediction':
     if st.button('Diabetes Test Result'):
         with st.spinner('Predicting...'):
             # Check if all fields are filled
-            if Pregnancies == '' or Glucose == '' or BloodPressure == '' or SkinThickness == '' or Insulin == '' or BMI == '' or DiabetesPedigreeFunction == '' or Age == '':
+            if st.session_state['Pregnancies'] == '' or st.session_state['Glucose'] == '' or st.session_state['BloodPressure'] == '' or st.session_state['SkinThickness'] == '' or st.session_state['Insulin'] == '' or st.session_state['BMI'] == '' or st.session_state['DiabetesPedigreeFunction'] == '' or st.session_state['Age'] == '':
                 st.warning('Please fill in all the input fields to get an accurate prediction.')
             else:
                 # Perform prediction
-                diab_prediction = diabetes_model.predict([[Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age]])
+                diab_prediction = diabetes_model.predict([[float(st.session_state['Pregnancies']), float(st.session_state['Glucose']), float(st.session_state['BloodPressure']), float(st.session_state['SkinThickness']), float(st.session_state['Insulin']), float(st.session_state['BMI']), float(st.session_state['DiabetesPedigreeFunction']), float(st.session_state['Age'])]])
                 st.session_state['features'] = {
-                    'Pregnancies': float(Pregnancies),
-                    'Glucose': float(Glucose),
-                    'Blood Pressure': float(BloodPressure),
-                    'Skin Thickness': float(SkinThickness),
-                    'Insulin': float(Insulin),
-                    'BMI': float(BMI),
-                    'Diabetes Pedigree Function': float(DiabetesPedigreeFunction),
-                    'Age': float(Age)
+                    'Pregnancies': float(st.session_state['Pregnancies']),
+                    'Glucose': float(st.session_state['Glucose']),
+                    'Blood Pressure': float(st.session_state['BloodPressure']),
+                    'Skin Thickness': float(st.session_state['SkinThickness']),
+                    'Insulin': float(st.session_state['Insulin']),
+                    'BMI': float(st.session_state['BMI']),
+                    'Diabetes Pedigree Function': float(st.session_state['DiabetesPedigreeFunction']),
+                    'Age': float(st.session_state['Age'])
                 }
                 st.session_state['diabetic'] = (diab_prediction[0] == 1)
                 if st.session_state['diabetic']:
@@ -159,21 +167,8 @@ if selected == 'Graphs/Charts':
 
         # Display detailed feature values below the charts
         st.write("### Detailed Feature Values:")
-        for name, value in zip(sorted_feature_names, sorted_feature_values):
-            st.write(f"- {name}: {value:.2f}")
+        for feature, value in feature_importance.items():
+            st.write(f"**{feature}**: {value}")
 
-        # Social media sharing with icons
-        st.markdown("""
-            ### Share your results!
-            <a href="https://twitter.com/intent/tweet?text=Check%20out%20my%20diabetes%20prediction%20results%20with%20Streamlit%20app!%20%23DiabetesPrediction%20%23Streamlit" target="_blank">
-            <img src="https://img.icons8.com/ios-filled/50/000000/twitter.png" alt="Twitter" style="vertical-align:middle; width: 30px; height: 30px;"/></a>
-            <a href="https://www.facebook.com/sharer/sharer.php?u=your_app_url" target="_blank">
-            <img src="https://img.icons8.com/ios-filled/50/000000/facebook.png" alt="Facebook" style="vertical-align:middle; width: 30px; height: 30px;"/></a>
-            <a href="https://www.linkedin.com/sharing/share-offsite/?url=your_app_url" target="_blank">
-            <img src="https://img.icons8.com/ios-filled/50/000000/linkedin.png" alt="LinkedIn" style="vertical-align:middle; width: 30px; height: 30px;"/></a>
-            <a href="https://wa.me/?text=Check%20out%20my%20diabetes%20prediction%20results%20with%20Streamlit%20app!%20%23DiabetesPrediction%20%23Streamlit" target="_blank">
-            <img src="https://img.icons8.com/ios-filled/50/000000/whatsapp.png" alt="WhatsApp" style="vertical-align:middle; width: 30px; height: 30px;"/></a>
-        """, unsafe_allow_html=True)
-        
     else:
-        st.warning('No prediction data available. Please make a prediction first.')
+        st.write("No prediction made yet. Please go to the 'Diabetes Prediction' page and submit your details for prediction.")
